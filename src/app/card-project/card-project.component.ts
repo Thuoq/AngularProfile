9ec41d098948a5acccc1424089731241f7f6976c
Thuoq <thuonghamno3xz@gmail.com>
2022-06-09 18:19:53 +0700

    add cv

--- src/app/card-project/card-project.component.ts
+++ src/app/card-project/card-project.component.ts
@@ -9,10 +9,28 @@ import {animate, style, transition, trigger} from '@angular/animations';
   animations: [
     trigger('scale', [
       transition('void => *', [
-        style({opacity: 0.2, transform: `scale(0.001)`}),
-        animate('0.2s 100ms ease-in-out', style({
+        style({
+          position: 'absolute',
+          top: '0px',
+          left: '0px',
+          visibility: 'hidden',
+          willChange: 'transform',
+          opacity: 0,
+          transitionDuration: '250ms',
+          transitionTimingFunction: `cubic-bezier(0.4, 0, 0.2, 1)`,
+          transitionProperty: `transform, opacity`,
+          transform: `translate(0px, 0px) scale(0.11)`,
+        }),
+        animate('0.25s', style({
+          position: 'absolute',
+          top: '0px',
+          left: '0px',
+          visibility: 'visible',
+          willChange: 'transform',
           opacity: 1,
-          transform: `scale(1)`
+          transitionTimingFunction: `cubic-bezier(0.4, 0, 0.2, 1)`,
+          transitionProperty: `transform, opacity`,
+          transform: `translate(0px, 0px) scale(1)`
         }))
       ])
     ])
